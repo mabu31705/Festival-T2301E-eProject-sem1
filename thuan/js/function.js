3e6520175c4e29872ce20a9927d7a08b1e66e876
mabu31705@gmail.com <mabu31705@gmail.com>
2023-06-05 16:21:00 +0700

    .

--- thuan/js/function.js
+++ thuan/js/function.js
@@ -1,4 +1,4 @@
-if (typeof(Storage) !== 'undefined') {
+if (typeof (Storage) !== 'undefined') {
     //Nếu có hỗ trợ
     //Thực hiện thao tác với Storage
     console.log('Trình duyệt của bạn hỗ trợ Storage');
@@ -6,57 +6,54 @@ if (typeof(Storage) !== 'undefined') {
     //Nếu không hỗ trợ
     console.log('Trình duyệt của bạn không hỗ trợ Storage');
 }
-let app = angular.module("myApp",["ngRoute"]);
-app.controller("firstController",myController );
+let app = angular.module("myApp", ["ngRoute"]);
+app.controller("firstController", myController);
 const container = document.getElementById('map')
-function myController($scope,$http){
-    $http.get("../data/data.json").
-    then(function (response){
+
+function myController($scope, $http) {
+    $http.get("../data/data.json").then(function (response) {
         $scope.books = response.data;
-        let data=$scope.books;
-        let christian = [$scope.books[0],$scope.books[1],$scope.books[2]];
-        let hindu = [$scope.books[3],$scope.books[4],$scope.books[5]];
-        let islam = [$scope.books[6],$scope.books[7],$scope.books[8]];
-        let p1=[];
-        let p2=[];
-        let p3=[];
-        let p4=[];
-        $scope.christian=christian;
-        $scope.hindu=hindu;
-        $scope.islam=islam;
-        for (let i=0; i<=$scope.books.length;i++) {
-            if (data[i].price <500){
-                p1.push(data[i]);
-            }
-            else if (data[i].price >500 && data[i].price <1000 ){
-                p2.push(data[i]);
-            }
-            else if (data[i].price >1000 && data[i].price <2000){
-                p3.push(data[i])
-            }
-            else if (data[i].price >2000 ){
-                p4.push(data[i])
-            }
-            $scope.p1=p1;
-            $scope.p2=p2;
-            $scope.p3=p3;
-            $scope.p4=p4;
-        }
+        let data = $scope.books;
+        let christian = [$scope.books[0], $scope.books[1], $scope.books[2]];
+        let hindu = [$scope.books[3], $scope.books[4], $scope.books[5]];
+        let islam = [$scope.books[6], $scope.books[7], $scope.books[8]];
+        let p1 = [];
+        let p2 = [];
+        let p3 = [];
+        let p4 = [];
+        $scope.christian = christian;
+        $scope.hindu = hindu;
+        $scope.islam = islam;
+        // for (let i = 0; i <= $scope.books.length; i++) {
+        //     if (data[i].price < 500) {
+        //         p1.push(data[i]);
+        //     } else if (data[i].price > 500 && data[i].price < 1000) {
+        //         p2.push(data[i]);
+        //     } else if (data[i].price > 1000 && data[i].price < 2000) {
+        //         p3.push(data[i])
+        //     } else if (data[i].price > 2000) {
+        //         p4.push(data[i])
+        //     }
+        //     $scope.p1 = p1;
+        //     $scope.p2 = p2;
+        //     $scope.p3 = p3;
+        //     $scope.p4 = p4;
+        // }
     })
-    let limit = 2;
-    let offset = 0;
-    let maxPage = 2;
-    $scope.limit = limit;
-    $scope.startOffset = offset;
-    $scope.pre = function(){
-        offset = offset - limit;
-        $scope.startOffset = offset;
-    }
-    $scope.next = function(){
-        offset = offset + limit;
-        $scope.startOffset = offset;
-    }
-    $scope.searchKey = "";
+    // let limit = 2;
+    // let offset = 0;
+    // let maxPage = 2;
+    // $scope.limit = limit;
+    // $scope.startOffset = offset;
+    // $scope.pre = function () {
+    //     offset = offset - limit;
+    //     $scope.startOffset = offset;
+    // }
+    // $scope.next = function () {
+    //     offset = offset + limit;
+    //     $scope.startOffset = offset;
+    // }
+    // $scope.searchKey = "";
 
     // map
 
@@ -78,26 +75,26 @@ function myController($scope,$http){
     // });
 
 }
-app.config(function ($routeProvider){
+
+app.config(function ($routeProvider) {
     $routeProvider
-        .when("/detail",{
-        templateUrl: "festival-shop-description.html"
-    })
-        .when("/books",{
-        templateUrl: "festival-shop.html"
-    })
-        .when("/abouts",{
-            templateUrl:"map.html"
-    })
+        .when("/detail", {
+            templateUrl: "festival-shop-description.html"
+        })
+        .when("/books", {
+            templateUrl: "festival-shop.html"
+        })
+        // .when("/abouts", {
+        //     templateUrl: "map.html"
+        // })
+        .when("/christ", {
+            templateUrl: "../../huy/html/ChristianFes.html"
+        })
+        .when("/hindu", {
+            templateUrl: "../../huy/html/HinduFes.html"
+        })
+        .when("/islam", {
+            templateUrl: "../../huy/html/IslamFes.html"
+        })
 
 })
-
-$(document).ready(function(){
-    $('.dropdown-submenu a.test').on("click", function(e){
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-    });
-});
-
-
