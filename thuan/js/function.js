84614214dd1304b6a2eee3941aac69053c3cf08e
mabu31705@gmail.com <mabu31705@gmail.com>
2023-06-12 12:48:31 +0700

    .

--- thuan/js/function.js
+++ thuan/js/function.js
@@ -7,14 +7,4 @@ if (typeof (Storage) !== 'undefined') {
     console.log('Trình duyệt của bạn không hỗ trợ Storage');
 }
 
-window.onload=function initMap() {
-    let lat= 21.028694544996597;
-    let lng= 105.78178303933102;
-    const map = L.map('map').setView([lat, lng], 15);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    const marker = L.marker([lat, lng]).addTo(map);
-}
--- thuan/module/landing/landing.component.js
+++ thuan/module/landing/landing.component.js
@@ -40,49 +40,49 @@ angular.module('landing').component('landing', {
                 {
                     src: './images/pngs/slide/1.jpg',
                     title: 'Christmas',
-                    des:'Christmas is an annual festival commemorating the birth of Jesus Christ, observed primarily on December 25[a] as a religious and cultural celebration among billions of people around the world.[2][3][4] A feast central to the Christian liturgical year, it is preceded by the season of Advent or the Nativity Fast and initiates the season of Christmastide, which historically in the West lasts twelve days and culminates on Twelfth Night.[5] Christmas Day is a public holiday in many countries,[6][7][8] is celebrated religiously by a majority of Christians,[9] as well as culturally by many non-Christians,[1][10] and forms an integral part of the holiday season organized around it.',
+                    des: 'Christmas is an annual festival commemorating the birth of Jesus Christ, observed primarily on December 25[a] as a religious and cultural celebration among billions of people around the world.[2][3][4] A feast central to the Christian liturgical year, it is preceded by the season of Advent or the Nativity Fast and initiates the season of Christmastide, which historically in the West lasts twelve days and culminates on Twelfth Night.[5] Christmas Day is a public holiday in many countries,[6][7][8] is celebrated religiously by a majority of Christians,[9] as well as culturally by many non-Christians,[1][10] and forms an integral part of the holiday season organized around it.',
                     button: 'Button 1'
                 },
                 {
                     src: './images/pngs/slide/2.jpg',
                     title: 'RamaNsita',
-                    des:'Sita Ramam is a 2022 Indian Telugu-language period romantic drama film written and directed by Hanu Raghavapudi. Produced by Vyjayanthi Movies and Swapna Cinema, the film stars Dulquer Salmaan, Mrunal Thakur, Rashmika Mandanna and Sumanth. The movie marks Mrunal Thakur\'s debut in Tollywood. Set in 1964, Lieutenant Ram, an orphaned army officer serving at the Kashmir border, gets anonymous love letters from Sita Mahalakshmi, after which Ram is on a mission to find Sita and propose his love.',
+                    des: 'Sita Ramam is a 2022 Indian Telugu-language period romantic drama film written and directed by Hanu Raghavapudi. Produced by Vyjayanthi Movies and Swapna Cinema, the film stars Dulquer Salmaan, Mrunal Thakur, Rashmika Mandanna and Sumanth. The movie marks Mrunal Thakur\'s debut in Tollywood. Set in 1964, Lieutenant Ram, an orphaned army officer serving at the Kashmir border, gets anonymous love letters from Sita Mahalakshmi, after which Ram is on a mission to find Sita and propose his love.',
                     button: 'Button 2'
                 },
                 {
                     src: './images/pngs/slide/3.jpg',
                     title: 'EidAlFitr',
-                    des:'Eid al-Fitr (/ˌiːd əl ˈfɪtər, -trə/; Arabic: عيد الفطر, romanized: ʿĪd al-Fiṭr, lit. \'Holiday of Breaking the Fast\',[4] IPA: [ʕiːd æl ˈfɪtˤr]) is the earlier of the two official holidays celebrated within Islam (the other being Eid al-Adha). While the Qur\'an does not mention the celebration of Eid,[5] the religious holiday of Eid al-Fitr is celebrated by Muslims worldwide because it marks the end of the month-long dawn-to-sunset fasting of Ramadan.[6] ',
+                    des: 'Eid al-Fitr (/ˌiːd əl ˈfɪtər, -trə/; Arabic: عيد الفطر, romanized: ʿĪd al-Fiṭr, lit. \'Holiday of Breaking the Fast\',[4] IPA: [ʕiːd æl ˈfɪtˤr]) is the earlier of the two official holidays celebrated within Islam (the other being Eid al-Adha). While the Qur\'an does not mention the celebration of Eid,[5] the religious holiday of Eid al-Fitr is celebrated by Muslims worldwide because it marks the end of the month-long dawn-to-sunset fasting of Ramadan.[6] ',
                     button: 'Button 3'
                 },
                 {
                     src: './images/pngs/slide/1.jpeg',
                     title: 'Easter',
-                    des:'Easter,[nb 1] also called Pascha[nb 2] (Aramaic, Greek, Latin) or Resurrection Sunday,[nb 3] is a Christian festival and cultural holiday commemorating the resurrection of Jesus from the dead, described in the New Testament as having occurred on the third day of his burial following his crucifixion by the Romans at Calvary c. 30 AD.[12][13] It is the culmination of the Passion of Jesus Christ, preceded by Lent (or Great Lent), a 40-day period of fasting, prayer, and penance.',
+                    des: 'Easter,[nb 1] also called Pascha[nb 2] (Aramaic, Greek, Latin) or Resurrection Sunday,[nb 3] is a Christian festival and cultural holiday commemorating the resurrection of Jesus from the dead, described in the New Testament as having occurred on the third day of his burial following his crucifixion by the Romans at Callety c. 30 AD.[12][13] It is the culmination of the Passion of Jesus Christ, preceded by Lent (or Great Lent), a 40-day period of fasting, prayer, and penance.',
                     button: 'Button 4'
                 },
                 {
                     src: './images/pngs/slide/2.jpeg',
                     title: 'Lent',
-                    des:'Lent (Latin: Quadragesima,[1] \'Fortieth\') is the solemn Christian religious observance in the liturgical year commemorating the 40 days Jesus Christ spent fasting in the desert and enduring temptation by Satan, according to the Gospels of Matthew, Mark and Luke, before beginning his public ministry.[2][3] Lent is observed in the Anglican, Eastern Orthodox, Lutheran, Methodist, Moravian, Oriental Orthodox, Church of the East, United Protestant and Roman Catholic traditions of Christianity',
+                    des: 'Lent (Latin: Quadragesima,[1] \'Fortieth\') is the solemn Christian religious observance in the liturgical year commemorating the 40 days Jesus Christ spent fasting in the desert and enduring temptation by Satan, according to the Gospels of Matthew, Mark and Luke, before beginning his public ministry.[2][3] Lent is observed in the Anglican, Eastern Orthodox, Lutheran, Methodist, Moravian, Oriental Orthodox, Church of the East, United Protestant and Roman Catholic traditions of Christianity',
                     button: 'Button 5'
                 },
                 {
                     src: './images/pngs/slide/3.jpeg',
                     title: 'Holi',
-                    des:'Holi (Hindi pronunciation: [\'hoːli:]) is a popular and significant Hindu festival celebrated as the Festival of Colours, Love and Spring.[1][9][10][11] It celebrates the eternal and divine love of the god Radha and Krishna.[12][13] Additionally, the day also signifies the triumph of good over evil,[14][15] as it commemorates the victory of Vishnu as Narasimha Narayana over Hiranyakashipu.[16][17] ',
+                    des: 'Holi (Hindi pronunciation: [\'hoːli:]) is a popular and significant Hindu festival celebrated as the Festival of Colours, Love and Spring.[1][9][10][11] It celebrates the eternal and divine love of the god Radha and Krishna.[12][13] Additionally, the day also signifies the triumph of good over evil,[14][15] as it commemorates the victory of Vishnu as Narasimha Narayana over Hiranyakashipu.[16][17] ',
                     button: 'Button 6'
                 },
                 {
                     src: './images/pngs/slide/4.jpeg',
                     title: 'HokumbhMela',
-                    des:'Kumbh Mela or Kumbha Mela (/ˌkʊmb ˈmeɪlə/) is a major pilgrimage and festival in Hinduism.[1] It is celebrated in a cycle of approximately 12 years, to celebrate every revolution Brihaspati (Jupiter) completes, at four river-bank pilgrimage sites: Prayagraj (Ganga-Yamuna-Sarasvati rivers confluence), Haridwar (Ganges), Nashik (Godavari), and Ujjain (Shipra).[1][2] ',
+                    des: 'Kumbh Mela or Kumbha Mela (/ˌkʊmb ˈmeɪlə/) is a major pilgrimage and festival in Hinduism.[1] It is celebrated in a cycle of approximately 12 years, to celebrate every revolution Brihaspati (Jupiter) completes, at four river-bank pilgrimage sites: Prayagraj (Ganga-Yamuna-Sarasvati rivers confluence), Haridwar (Ganges), Nashik (Godaleti), and Ujjain (Shipra).[1][2] ',
                     button: 'Button 6'
                 },
                 {
                     src: './images/pngs/slide/5.jpeg',
-                    title: 'Mahashivaratri',
-                    des:'Maha Shivaratri (Sanskrit: महाशिवरात्रि, romanized: Mahāśivarātri, lit. \'The Great Night of Shiva\') is a Hindu festival celebrated annually in honour of the deity Shiva, between February and March.[7] According to the Hindu calendar, the festival is observed on the fourteenth day of the dark (waning) half of the lunar month of Phalguna or Magha.[7][8] ',
+                    title: 'Mahashiletatri',
+                    des: 'Maha Shiletatri (Sanskrit: महाशिवरात्रि, romanized: Mahāśiletātri, lit. \'The Great Night of Shiva\') is a Hindu festival celebrated annually in honour of the deity Shiva, between February and March.[7] According to the Hindu calendar, the festival is observed on the fourteenth day of the dark (waning) half of the lunar month of Phalguna or Magha.[7][8] ',
                     button: 'Button 6'
                 }
             ];
@@ -109,7 +109,7 @@ angular.module('landing').component('landing', {
                 slideshow.innerHTML = '';
                 slideshow.appendChild(slide);
                 imageDescription.textContent = image.title;
-                des.textContent=image.des;
+                des.textContent = image.des;
             }
 
             function updateActiveDot(index) {
@@ -136,10 +136,11 @@ angular.module('landing').component('landing', {
                 nextSlide(); // Show the initial slide
 
                 // Start the interval timer
-                setInterval(function() {
+                setInterval(function () {
                     nextSlide();
                 }, 3000); // Change slide every 3 seconds
             }
+
             // Create dots for each slide
             for (let i = 0; i < images.length; i++) {
                 let dot = document.createElement('div');
@@ -147,7 +148,7 @@ angular.module('landing').component('landing', {
                 if (i === currentIndex) {
                     dot.classList.add('activeSlide');
                 }
-                dot.addEventListener('click', function() {
+                dot.addEventListener('click', function () {
                     let dotIndex = parseInt(this.dataset.index);
                     createSlide(dotIndex);
                     updateActiveDot(dotIndex);
@@ -164,6 +165,46 @@ angular.module('landing').component('landing', {
             nextBtn.addEventListener('click', nextSlide);
 
             startSlideshow();
+
+            //  Set the date we're counting down to
+            const date = new Date();
+
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
+
+            // This arrangement can be altered based on how we want the date's format to appear.
+            let currentDate = `${day}-${month}-${year}`;
+            let countDownDate = new Date(currentDate).getTime();
+
+            // Update the count down every 1 second
+            let x = setInterval(function () {
+
+                // Get today's date and time
+                let now = new Date().getTime();
+
+                // Find the distance between now and the count down date
+                let distance = countDownDate - now;
+
+                // Time calculations for days, hours, minutes and seconds
+                let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                // Output the result in an element with id="demo"
+                document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+                    + minutes + "m " + seconds + "s ";
+                document.getElementById("days").innerHTML = days;
+                document.getElementById("hours").innerHTML = hours;
+                document.getElementById("minutes").innerHTML = minutes;
+                document.getElementById("seconds").innerHTML = seconds;
+                // If the count down is over, write some text
+                if (distance < 0) {
+                    clearInterval(x);
+                    document.getElementById("demo").innerHTML = "EXPIRED";
+                }
+            }, 1000);
 
         }
     ]
